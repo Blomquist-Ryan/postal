b23560e5e6afad4d622e954a0051ec1332ae3057
Ryan Blomquist <rblomquist96@gmail.com>
2019-06-21 16:27:32 -0600

    typo

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ express()
   .set('public', path.join(__dirname, 'public'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/from', (req, res) => res.render('public/from'))
+  .get('/from', (req, res) => res.render('views'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
   function form(){
