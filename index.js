b7a528cc8fc82151aae90dfb71949799d60adef9
Ryan Blomquist <rblomquist96@gmail.com>
2019-06-21 17:47:55 -0600

    things

--- index.js
+++ index.js
@@ -23,16 +23,68 @@ express()
     const type = req.query.package;
     const weight = req.query.weight;
 
-    //computeRate(Response, type, weight);
+    computeRate(Response, type, weight);
 
 
 
-    const params = {type: type, weight: weight};
-    res.render('pages/result', params);
+   
 
   }
 
 function computeRate(Response, type, weight){
+  const price = 0;
 
+  if(type == "stamped"){
+    if(wieght < 1){
+      price = .55;
+    } else if(weight < 3.5){
+      price = .55 + ((weight - 1) * .15);
+
+    } else {
+      price = 1;
+    }
+    type = "stamped letter";
+
+  } else if(type == "metered"){
+    if(wieght < 1){
+      price = .50;
+    } else if(weight < 3.5){
+      price = .50 + ((weight - 1) *.15);
+
+    } else {
+      price = 1;
+    }
+
+    type = "metered letter";
+
+  } else if(type == "flat"){
+    if(wieght < 1){
+      price = 1;
+    } else if(weight < 13){
+      price = 1 + (((weight-1) *.15));
+
+    } else {
+      price = 2.80;
+    }
+    type = "large envelopes";
+
+
+  } else if(type == "retail"){
+    if(wieght < 4){
+      price = 3.66;
+    } else if(weight < 8){
+      price = 4.39;
+
+    } else if(weight < 12){
+      price = 4.19;
+
+    } else {
+      price = 5.71;
+    }
+    type = "First-Class package";
+  }
+
+ const params = {type: type, weight: weight, price: price};
+    res.render('pages/result', params);
 }
   
